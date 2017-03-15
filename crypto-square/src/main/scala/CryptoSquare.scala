964c1c86449908c02d1469df7159e3a1eb9b626f
Kim Daesap <sapzil78@gmail.com>
2017-03-16 01:46:47 +0900

    crypto square 푸는 중

--- crypto-square/src/main/scala/CryptoSquare.scala
+++ crypto-square/src/main/scala/CryptoSquare.scala
@@ -1,11 +1,20 @@
 object CryptoSquare {
-  def normalizePlaintext(text: String): String = ???
+  def normalizePlaintext(text: String): String =
+    text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase
 
-  def squareSize(text: String): Int = ???
+  def squareSize(text: String): Int =
+    (Math.sqrt(text.length) + 0.9).toInt
 
-  def plaintextSegments(text: String): List[String] = ???
+  def plaintextSegments(text: String): List[String] =
+    normalizePlaintext(text).grouped(squareSize(text)).toList
 
-  def ciphertext(text: String): String = ???
+  def ciphertext(text: String): String = {
+    val ps = plaintextSegments(text)
+    val l = squareSize(text)
+    ps.map(_.padTo(l, "")).transpose.map(_.mkString).mkString
+  }
 
-  def normalizedCiphertext(text: String): String = ???
+  def normalizedCiphertext(text: String): String = {
+    ""
+  }
 }

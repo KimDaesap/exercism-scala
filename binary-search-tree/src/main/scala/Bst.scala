a3a3790ca9dfc11b323ff0bf4ce99b1cd6aa9188
DESKTOP-5QFAML5\admin <sapzil78jw@gmail.com>
2017-03-27 17:48:15 +0900

    binary-search-tree working

--- binary-search-tree/src/main/scala/Bst.scala
+++ binary-search-tree/src/main/scala/Bst.scala
@@ -4,14 +4,13 @@ case class Bst(value: Int, val left: Option[Bst], val right: Option[Bst]) {
     def loop(cursor: Option[Bst], in: Int): Bst = {
       cursor match {
         case None => Bst(in)
-        case Some(x) if x.value > in => Bst(x.value, Some(loop(x.left, in)), x.right)
-        case _ => Bst(0)
+        case Some(x) =>
+          if (x.value >= in) Bst(x.value, Some(loop(x.left, in)), x.right)
+          else Bst(x.value, x.left, Some(loop(x.left, in)))
       }
     }
 
     loop(Some(this), input)
-
-    Bst(1, None, None)
   }
 }
 

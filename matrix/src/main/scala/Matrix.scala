400de54a2c24c46b1da2d0d0bd829f625faf0558
DESKTOP-5QFAML5\admin <sapzil78jw@gmail.com>
2017-03-31 11:06:42 +0900

    matrix improve

--- matrix/src/main/scala/Matrix.scala
+++ matrix/src/main/scala/Matrix.scala
@@ -1,13 +1,12 @@
-case class Matrix(text: String) {
-  val matrix = text
-    .split('\n')
-    .map(_.split(' ').map(_.toInt).toVector)
-    .toVector
-
-  def rows(row: Int): Vector[Int] =
-    matrix.apply(row)
-
-  def cols(col: Int): Vector[Int] =
-    matrix.transpose.apply(col)
+case class Matrix(rows: Vector[Vector[Int]]) {
+  def cols(col: Int): Vector[Int] = rows.transpose.apply(col)
 }
 
+object Matrix {
+ def apply(text: String): Matrix ={
+   Matrix(text
+     .split('\n')
+     .map(_.split(' ').map(_.toInt).toVector)
+     .toVector)
+ }
+}

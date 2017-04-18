3e481a71fe6927fc8950ab899a01d3708345e039
Choi sangmin <sapzil78jw@gmail.com>
2017-04-18 10:35:43 +0900

    sum-of-multiples improve

--- sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,15 +1,8 @@
 object SumOfMultiples {
   def sumOfMultiples(factors: Set[Int], limit: Int): Int = {
-    def loop(f: Int, m: Int, acc: List[Int], limit: Int): List[Int] = {
-      val x = f + m
-      if (x < limit) loop(f, x, x :: acc, limit)
-      else acc
-    }
-
-    { for {f <- factors
-        fs = loop(f, 0, Nil, limit)
-      } yield fs
-    }.flatten.sum
+    (1 until limit).flatMap(x =>
+      if (factors.exists(ft => x % ft == 0)) Some(x)
+      else None ).sum
   }
 }
 

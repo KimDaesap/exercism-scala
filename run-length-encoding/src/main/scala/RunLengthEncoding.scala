5ab7753ee9c7d47952a80f7c89910c272a0b1509
Choi sangmin <sapzil78jw@gmail.com>
2017-04-21 11:15:17 +0900

    run-length-encoding IMPROVE

--- run-length-encoding/src/main/scala/RunLengthEncoding.scala
+++ run-length-encoding/src/main/scala/RunLengthEncoding.scala
@@ -1,35 +1,23 @@
 object RunLengthEncoding {
 
-  def encode(str: String): String = {
-    def loop(xs: String, acc: String): String = {
-      xs match {
-        case "" => acc
-        case t => {
-          val char = t.head
-          val (left, right) = t.span(_ == char)
-          val lt = if (left.length > 1) left.length.toString else ""
-          loop(right, acc + lt + char)
-        }
-      }
+  def encode(str: String): String = str match {
+    case "" => ""
+    case t => {
+      val char = t.head
+      val (same, remains) = t.span(_ == char)
+      val lt = if (same.length > 1) same.length.toString else ""
+      lt + char + encode(remains)
     }
-
-    loop(str, "")
   }
 
-  def decode(str: String): String = {
-    def loop(xs: String, acc: String): String = {
-      xs match {
-        case "" => acc
-        case t => {
-          val (lt, dt) = t.span(_.isDigit)
-          val l = if (lt == "") 1 else lt.toInt
-          val (char, remains) = dt.splitAt(1)
-          loop(remains, acc + char * l)
-        }
-      }
+  def decode(str: String): String = str match {
+    case "" => ""
+    case t => {
+      val (lt, dt) = t.span(_.isDigit)
+      val l = if (lt == "") 1 else lt.toInt
+      val (char, remains) = dt.splitAt(1)
+      char * l + decode(remains)
     }
-
-    loop(str, "")
   }
 }
 

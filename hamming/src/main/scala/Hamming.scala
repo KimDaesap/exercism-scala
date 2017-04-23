7f2f92828432d345441580fbbb54beca79869e6c
Kim Daesap <sapzil78@gmail.com>
2017-04-23 11:47:32 +0900

    hamming IMPROVE

--- hamming/src/main/scala/Hamming.scala
+++ hamming/src/main/scala/Hamming.scala
@@ -2,16 +2,16 @@
 object Hamming {
 
   def compute(left: String, right: String): Option[Int] = {
-    def loop(x: List[Char], y: List[Char], acc: Int): Option[Int] = {
+    def loop(x: Seq[Char], y: Seq[Char], acc: Int): Option[Int] = {
       (x, y) match {
-        case (Nil, Nil) => Some(acc)
-        case (lh :: lt, rh :: rt) =>
+        case (Seq(), Seq()) => Some(acc)
+        case (lh +: lt, rh +: rt) =>
           loop(lt, rt, if (lh == rh) acc else acc + 1)
         case _ => None
       }
     }
 
-    loop(left.toList, right.toList, 0)
+    loop(left, right, 0)
   }
 
 }

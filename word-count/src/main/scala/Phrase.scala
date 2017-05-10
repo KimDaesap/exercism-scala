920844447a5f2e996a5effc9163f856efbc1196f
Choi sangmin <sapzil78jw@gmail.com>
2017-05-10 10:21:53 +0900

    word-count done

--- word-count/src/main/scala/Phrase.scala
+++ word-count/src/main/scala/Phrase.scala
@@ -1,38 +1,11 @@
 
 case class Phrase(text: String) {
   def wordCount: Map[String, Int] = {
-    val xs = text.split(' ').map(_.toLowerCase).groupBy(identity)
-    val ys = xs.map { case (k, vs) => (k, vs.length) }
-    ys
+    text
+      .toLowerCase
+      .filterNot(c => ":!&@$%^&.".contains(c))
+      .split("[ ,]+")
+      .groupBy(identity)
+      .map(t => (t._1, t._2.length))
   }
 }
-
-/*
-case class Phrase(text: String) {
-  def wordCount: Map[String, Int] = {
-    val filtered = text.filterNot(c => ":!!&@$%^&".contains(c))
-    println(filtered)
-    val xs = filtered.split(' ').map(_.toLowerCase).groupBy(identity)
-    val ys = xs.map { case (k, vs) => (k, vs.length) }
-    ys
-  }
-}
-
-
-val input = "car : carpet as java : javascript!!&@$%^&"
-
-val result = input.foldLeft(("", false)) { (acc, c) =>
-  val (output, skipped) = acc
-
-  ":!!&@$%^&".contains(c) || c == ' ' && skipped
-
-
-  c match {
-    case c if ":!!&@$%^&".contains(c) => (output, true)
-    case c if c == ' ' && skipped => (output, true)
-    case _ => (output + c, false)
-  }
-}
-
-result._1
- */

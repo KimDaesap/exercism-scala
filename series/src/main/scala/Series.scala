af31e6ac940d2a9548e2929ebec9646add78126e
Choi sangmin <sapzil78jw@gmail.com>
2017-05-19 11:40:03 +0900

    series done

--- series/src/main/scala/Series.scala
+++ series/src/main/scala/Series.scala
@@ -1,5 +1,8 @@
 object Series {
   def slices(step: Int, text: String): List[List[Int]] = {
-    text.sliding(step,1).map(str => str.map(ch => ch.asDigit).toList).toList
+    text
+      .sliding(step, 1)
+      .map(str => str.map(ch => ch.asDigit).toList)
+      .toList
   }
 }

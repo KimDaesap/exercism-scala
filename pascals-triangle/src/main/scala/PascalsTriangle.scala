d85442662af86e3815923ed31560d2a2d887d79b
Choi sangmin <sapzil78jw@gmail.com>
2017-05-22 12:37:04 +0900

    pascals-triangle improve

--- pascals-triangle/src/main/scala/PascalsTriangle.scala
+++ pascals-triangle/src/main/scala/PascalsTriangle.scala
@@ -3,10 +3,8 @@ object PascalsTriangle {
     def loop(curLv: Int, toLv: Int, prev: List[Int]): List[List[Int]] = {
       if (curLv > toLv) Nil
       else {
-        val mid = if (prev.length == 1) Nil
-        else prev.sliding(2, 1).map(_.sum).toList
-
-        prev :: loop(curLv + 1, toLv, 1 :: mid ::: List(1))
+        val next = (0 :: prev) zip (prev :+ 0) map { case (x, y) => x + y }
+        prev :: loop(curLv + 1, toLv, next)
       }
     }
     loop(1, level, List(1))

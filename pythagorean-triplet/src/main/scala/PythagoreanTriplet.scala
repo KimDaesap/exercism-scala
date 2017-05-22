f439368c1178d5401410ceff719bd71210ca8c9e
Choi sangmin <sapzil78jw@gmail.com>
2017-05-22 14:08:56 +0900

    pythahorean-triplet working

---
+++ pythagorean-triplet/src/main/scala/PythagoreanTriplet.scala
@@ -0,0 +1,10 @@
+object PythagoreanTriplet {
+  def pythagoreanTriplets(i: Int, i1: Int): Seq[(Int, Int, Int)] = {
+    ???
+  }
+
+  def isPythagorean(tuple: (Int, Int, Int)): Boolean = {
+    ???
+  }
+
+}

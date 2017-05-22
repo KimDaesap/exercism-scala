652b109dc20e7607a51c723185bcb43265b45291
Choi sangmin <sapzil78jw@gmail.com>
2017-05-22 12:19:46 +0900

    pascals-triangle done

--- pascals-triangle/src/main/scala/PascalsTriangle.scala
+++ pascals-triangle/src/main/scala/PascalsTriangle.scala
@@ -1,7 +1,14 @@
 object PascalsTriangle {
+  def triangle(level: Int): List[List[Int]] = {
+    def loop(curLv: Int, toLv: Int, prev: List[Int]): List[List[Int]] = {
+      if (curLv > toLv) Nil
+      else {
+        val mid = if (prev.length == 1) Nil
+        else prev.sliding(2, 1).map(_.sum).toList
 
-  def triangle(number: Int): List[List[Int]] = {
-    ???
+        prev :: loop(curLv + 1, toLv, 1 :: mid ::: List(1))
+      }
+    }
+    loop(1, level, List(1))
   }
-
 }

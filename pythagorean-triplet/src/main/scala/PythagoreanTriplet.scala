ba255e7ce17f99bd2a2c91cb191b0007ff3a2bd6
Choi sangmin <sapzil78jw@gmail.com>
2017-05-26 11:21:13 +0900

    pythahorean-triplet done

--- pythagorean-triplet/src/main/scala/PythagoreanTriplet.scala
+++ pythagorean-triplet/src/main/scala/PythagoreanTriplet.scala
@@ -1,10 +1,15 @@
 object PythagoreanTriplet {
-  def pythagoreanTriplets(i: Int, i1: Int): Seq[(Int, Int, Int)] = {
-    ???
+  def pythagoreanTriplets(start: Int, end: Int): Seq[(Int, Int, Int)] = {
+    for {
+      a <- (start to end)
+      b <- (a to end)
+      c <- (b to end)
+      if isPythagorean(a,b,c)
+    } yield (a,b,c)
   }
 
   def isPythagorean(tuple: (Int, Int, Int)): Boolean = {
-    ???
+    val sorted = tuple match { case (a,b,c) => List(a,b,c).sorted }
+    sorted match { case List(a, b, c) => a * a + b * b == c * c }
   }
-
 }

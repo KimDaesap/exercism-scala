417faad3fd8dd731273e9247d746ab1cb66ffefb
Choi sangmin <sapzil78jw@gmail.com>
2017-05-29 14:09:12 +0900

    * acronym 문제 완료

--- acronym/src/main/scala/Acronym.scala
+++ acronym/src/main/scala/Acronym.scala
@@ -1,3 +1,17 @@
 object Acronym {
-  def abbreviate(phrase: String): String = ???
+  def isSeparates(lc: Char, rc: Char): Boolean =
+    lc == ' ' || lc == '-' || (lc.isLower && rc.isUpper)
+
+  def abbreviate(phrase: String): String = {
+    def loop(prev: Char, text: Seq[Char]): String = {
+      text match {
+        case Seq() => ""
+        case h +: t =>
+          if (isSeparates(prev, h)) h.toUpper + loop(h, t)
+          else loop(h, t)
+      }
+    }
+
+    loop(' ', phrase)
+  }
 }

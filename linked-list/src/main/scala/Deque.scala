dd83c26940132c5df6f383b436e07debd4b62a86
Choi sangmin <sapzil78jw@gmail.com>
2017-06-13 10:30:40 +0900

    * linked-list 문제 개선  - head, tail에 대한 getter, setter 선언을 통한 가독성 개선

--- linked-list/src/main/scala/Deque.scala
+++ linked-list/src/main/scala/Deque.scala
@@ -1,59 +1,61 @@
 class Deque[A] {
 
-  type TNode = Option[Node]
+  type NodeOp = Option[Node]
 
   sealed trait Node{
-    var prev: TNode
-    var next: TNode
+    var prev: NodeOp
+    var next: NodeOp
   }
 
-  case class EndNode(var prev: TNode, var next: TNode) extends Node
-  case class ValueNode(var prev: TNode, value: A, var next: TNode) extends Node
+  case class EndNode(var prev: NodeOp, var next: NodeOp) extends Node
+  case class ValueNode(var prev: NodeOp, value: A, var next: NodeOp) extends Node
 
-  var end: TNode = Some(EndNode(None, None))
+  var end: NodeOp = Some(EndNode(None, None))
   end.foreach(en => {
     en.prev = end
     en.next = end
   })
 
+  def head = end.flatMap(_.next)
+  def head_= (node: NodeOp) = end.foreach(en => en.next = node)
+  def tail = end.flatMap(_.prev)
+  def tail_= (node: NodeOp) = end.foreach(en => en.prev = node)
+
   def unshift(value: A): Unit = {
-    val node: TNode = Some(ValueNode(end, value, end.flatMap(en => en.next)))
-    end.foreach(en => {
-      en.next.foreach(fn => fn.prev = node)
-      en.next = node
-    })
+    val node: NodeOp = Some(ValueNode(end, value, head))
+    head.foreach(_.prev = node)
+    head = node
   }
 
   def shift: Option[A] = {
-    end.flatMap(en => en.next.flatMap(fn => fn match {
+    head.flatMap(_ match {
       case _: EndNode => None
-      case fn: ValueNode =>
-        en.next = fn.next
-        fn.next.foreach(sn => sn.prev = end)
-        Some(fn.value)
-    }))
-  }
-
-  def push(value: A): Unit = {
-    val node: TNode = Some(ValueNode(end.flatMap(en => en.prev), value, end))
-    end.foreach(en => {
-      en.prev.foreach(ln => ln.next = node)
-      en.prev = node
+      case hn: ValueNode =>
+        head = hn.next
+        hn.next.foreach(_.prev = end)
+        Some(hn.value)
     })
   }
 
-  def pop: Option[A] = {
-    end.flatMap(en => en.prev.flatMap(ln => ln match {
-      case _: EndNode => None
-      case ln: ValueNode =>
-        en.prev = ln.prev
-        ln.prev.foreach(sn => sn.next = end)
-        Some(ln.value)
-    }))
+  def push(value: A): Unit = {
+    val node: NodeOp = Some(ValueNode(tail, value, end))
+    tail.foreach(_.next = node)
+    tail = node
   }
 
+  def pop: Option[A] = {
+    tail.flatMap(_ match {
+      case _: EndNode => None
+      case tn: ValueNode =>
+        tail = tn.prev
+        tn.prev.foreach(_.next = end)
+        Some(tn.value)
+    })
+  }
+
+  // Debug method
   def print: String = {
-    def loop(node: TNode): String = {
+    def loop(node: NodeOp): String = {
       node.map(n => n.next match {
         case Some(nn: ValueNode) => nn.value.toString + "," + loop(n.next)
         case _ => "end"

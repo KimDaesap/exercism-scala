7cce5dba4a1f269024c5d45f392524e6970002ee
Choi sangmin <sapzil78jw@gmail.com>
2017-06-16 17:42:48 +0900

    # custom-set 문제 - 개선1 - Red black tree 기반 Set으로 변경중

--- custom-set/src/main/scala/CustomSet.scala
+++ custom-set/src/main/scala/CustomSet.scala
@@ -1,36 +1,31 @@
-class CustomSet(var list: List[Int]) { list = list.sorted }
+class CustomSet(val tree: Tree) {
+  def this(list: List[Int]) { this(Tree.fromList(list.sorted)) }
+}
 
 object CustomSet {
-  def apply(): CustomSet = new CustomSet(List())
+  def apply(): CustomSet = new CustomSet(Nil)
 
   def fromList(list: List[Int]): CustomSet = new CustomSet(list)
 
-  def empty(set: CustomSet): Boolean = set.list.isEmpty
+  def member(set: CustomSet, n: Int): Boolean = set.tree.contains(n)
+
+  def empty(set: CustomSet): Boolean = set.tree.isEmpty
 
   def isEqual(left: CustomSet, right: CustomSet): Boolean =
-    left.list.equals(right.list)
+    left.tree.Equals(right.tree)
 
   def isSubsetOf(left: CustomSet, right: CustomSet): Boolean =
-    right.list.containsSlice(left.list)
+    right.tree.isSubset(left.tree)
 
   def isDisjointFrom(left: CustomSet, right: CustomSet): Boolean =
-    left.list.forall(!right.list.contains(_))
+    left.tree.isDisjoint(right.tree)
 
-  def member(set: CustomSet, n: Int): Boolean =
-    set.list.contains(n)
+  def insert(set: CustomSet, n: Int): CustomSet = new CustomSet(set.tree.insert(n))
 
-  def insert(set: CustomSet, n: Int): CustomSet =
-    fromList(if (!set.list.contains(n)) n :: set.list else set.list)
+  def intersection(left: CustomSet, right: CustomSet): CustomSet = ???
 
-  def intersection(left: CustomSet, right: CustomSet): CustomSet =
-    fromList(left.list.filter(right.list.contains(_)))
+  def difference(left: CustomSet, right: CustomSet): CustomSet = ???
 
-  def difference(left: CustomSet, right: CustomSet): CustomSet =
-    fromList(left.list.filter(!right.list.contains(_)))
-
-  def union(left: CustomSet, right: CustomSet): CustomSet = right.list match {
-    case Nil => left
-    case h :: t => union(insert(left, h), fromList(t))
-  }
+  def union(left: CustomSet, right: CustomSet): CustomSet = ???
 
 }

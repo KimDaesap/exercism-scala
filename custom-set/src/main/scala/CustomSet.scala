53d6f2730d711db2adf70b540e605c7255c1245e
Choi sangmin <sapzil78jw@gmail.com>
2017-06-19 17:05:00 +0900

    # custom-set 문제 - 개선1 - Red black tree를 사용했을 때 문제가 있음 - 같은 값이 들어 있는 tree라도 순서와 구조가 다를 수 있음 - tree 간에 비교가 힘들다! - 일단 list로 변환후 처리... 뭔가 tree를 쓴 의미가 없다. - avl tree를 사용하면 해결 가능할 것으로 생각!

--- custom-set/src/main/scala/CustomSet.scala
+++ custom-set/src/main/scala/CustomSet.scala
@@ -1,5 +1,69 @@
+sealed trait Color
+case object Black extends Color
+case object Red extends Color
+
+case object Empty extends Tree { def color = Black }
+case class Node(color: Color, left: Tree, value: Int, right: Tree) extends Tree
+case object Leaf extends Tree { def color = Black }
+
+sealed trait Tree {
+  def color: Color
+
+  def isEmpty: Boolean = this == Empty
+
+  def contains(x: Int): Boolean = this match {
+    case Node(c, l, v, r) =>
+      if (x < v) l.contains(x)
+      else if (v < x) r.contains(x)
+      else true
+    case _ => false
+  }
+
+  def list: List[Int] = this match {
+    case Node(c, l, v, r) => l.list ::: v :: r.list
+    case _ => Nil
+  }
+
+  def insert(x: Int): Tree = add(this, x) match {
+    case t: Node => t.copy(color = Black)
+    case _ => Empty
+  }
+
+  private def add(t: Tree, x: Int): Tree = t match {
+    // BST insertion logic base + RB balance
+    case Node(c, l, v, r) =>
+      if (x < v) balance(Node(c, add(l, x), v, r))
+      else if (v < x) balance(Node(c, l, v, add(r, x)))
+      else Node(c, l, v, r)
+    case Leaf => Node(Red, Leaf, x, Leaf)
+    // first insert (case 1)
+    case Empty => Node(Black, Leaf, x, Leaf)
+  }
+
+  private def balance(tree: Tree): Tree = tree match {
+    case Node(Black, Node(Red, Node(Red, a, x, b), y, c), z, d) =>
+      Node(Red, Node(Black, a, x, b), y, Node(Black, c, z, d))
+    case Node(Black, Node(Red, a, x, Node(Red, b, y, c)), z, d) =>
+      Node(Red, Node(Black, a, x, b), y, Node(Black, c, z, d))
+    case Node(Black, a, x, Node(Red, b, y, Node(Red, c, z, d))) =>
+      Node(Red, Node(Black, a, x, b), y, Node(Black, c, z, d))
+    case Node(Black, a, x, Node(Red, Node(Red, b, y, c), z, d)) =>
+      Node(Red, Node(Black, a, x, b), y, Node(Black, c, z, d))
+    case _ => tree
+  }
+}
+
+object Tree {
+  def apply(): Tree = Empty
+
+  def fromList(list: List[Int]): Tree = {
+    list.foldLeft(Tree()) ((acc, x) => acc.insert(x))
+  }
+}
+
 class CustomSet(val tree: Tree) {
   def this(list: List[Int]) { this(Tree.fromList(list.sorted)) }
+  def list: List[Int] = tree.list
 }
 
 object CustomSet {
@@ -12,20 +76,24 @@ object CustomSet {
   def empty(set: CustomSet): Boolean = set.tree.isEmpty
 
   def isEqual(left: CustomSet, right: CustomSet): Boolean =
-    left.tree.equals(right.tree)
+    left.list == right.list
 
   def isSubsetOf(left: CustomSet, right: CustomSet): Boolean =
-    right.tree.isSubset(left.tree)
+    right.list.containsSlice(left.list)
 
   def isDisjointFrom(left: CustomSet, right: CustomSet): Boolean =
-    left.tree.isDisjoint(right.tree)
+    left.list.forall(!right.tree.contains(_))
 
   def insert(set: CustomSet, n: Int): CustomSet = new CustomSet(set.tree.insert(n))
 
-  def intersection(left: CustomSet, right: CustomSet): CustomSet = ???
+  def intersection(left: CustomSet, right: CustomSet): CustomSet =
+    fromList(left.list.filter(right.tree.contains(_)))
 
-  def difference(left: CustomSet, right: CustomSet): CustomSet = ???
+  def difference(left: CustomSet, right: CustomSet): CustomSet =
+    fromList(left.list.filter(!right.tree.contains(_)))
 
-  def union(left: CustomSet, right: CustomSet): CustomSet = ???
-
+  def union(left: CustomSet, right: CustomSet): CustomSet = right.list match {
+    case Nil => left
+    case h :: t => union(insert(left, h), fromList(t))
+  }
 }

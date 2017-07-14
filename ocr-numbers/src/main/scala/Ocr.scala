cb320249d5352e253fe53f4eb64743d2263b631f
Choi SangMin <sapzil78jw@gmail.com>
2017-07-14 11:25:45 +0900

    # ocr-numbers 문제 풀이 2

--- ocr-numbers/src/main/scala/Ocr.scala
+++ ocr-numbers/src/main/scala/Ocr.scala
@@ -10,39 +10,19 @@ case class Ocr(text: String) {
     " _   |  |   " -> "7",
     " _ |_||_|   " -> "8",
     " _ |_| _|   " -> "9"
-  )
+  ).withDefaultValue("?")
 
   def convert: String = {
+    def toDigit(line: Seq[String]): String = line
+      .map(_.grouped(3).toSeq)
+      .transpose
+      .map(ts => digits(ts.mkString))
+      .mkString
 
-    def line(t: String): (List[String], String) = {
-      if (t.startsWith("\n")) (Nil, t.tail)
-      else if(t.isEmpty) (Nil, t)
-      else {
-        val (l, r) = t.splitAt(3)
-        val (ls, r2) = line(r)
-        (l :: ls, r2)
-      }
-    }
-
-    def line4(n: Int, t: String, acc: List[String]): (List[String], String) = {
-      if (n > 0) {
-        val (ls, r) = line(t)
-        line4(n - 1, r,
-          acc.zipAll(ls, "", "").map { case (a,b) => a + b }
-        )
-      }
-      else (acc, t)
-    }
-
-    def chunk(t: String, acc: String): String = {
-      if (t.isEmpty) acc
-      else {
-        val (ls, r) = line4(4, t, Nil)
-        acc + (if (acc.isEmpty) "" else ",") +
-          chunk(r, ls.map(a => digits.getOrElse(a, "?")).mkString)
-      }
-    }
-
-    chunk(text, "")
+    text
+      .split("\n")
+      .grouped(4)
+      .map(toDigit(_))
+      .mkString(",")
   }
 }

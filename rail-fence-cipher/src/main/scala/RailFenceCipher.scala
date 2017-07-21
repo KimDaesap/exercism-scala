344991db5340778a0b3aa937e0c3b70ad59310f4
Choi SangMin <sapzil78@gmail.com>
2017-07-21 18:03:02 +0900

    # rail-fence-cipher 문제 푸는 중

--- rail-fence-cipher/src/main/scala/RailFenceCipher.scala
+++ rail-fence-cipher/src/main/scala/RailFenceCipher.scala
@@ -1,23 +1,19 @@
+import scala.collection.mutable.ListBuffer
+
 object RailFenceCipher {
-
-  def encode(text: String, depth: Int): String = {
-    import scala.collection.mutable.SortedMap
-
-    def go(ts: Seq[Char], level: Int, factor: Int, map: SortedMap[Int, String]): SortedMap[Int, String] = {
-      ts match {
-        case Seq() => map
-        case h +: t =>
-          val nf = if (level == 1) 1
-          else if (level >= (depth)) -1
-          else factor
-
-          map(level) = map.getOrElse(level, "") + h
-          go(t, level + nf, nf, map)
+  def encode(text: String, railNumber: Int): String = {
+    def go(ts: String, number: Int, increment: Int, rails: ListBuffer[String]): ListBuffer[String] = {
+      if (ts.isEmpty) rails
+      else {
+        val inc = if (number == 0) 1
+        else if (number >= (railNumber - 1)) -1
+        else increment
+        rails(number) = rails(number) + ts.head
+        go(ts.tail, number + inc, inc, rails)
       }
     }
 
-    go(text, 1, 1, SortedMap())
-      .foldLeft("")((acc, tp) => acc + tp._2)
+    go(text, 0, 1, ListBuffer.fill(railNumber)("")).mkString
   }
 
   def decode(text: String, depth: Int): String = {

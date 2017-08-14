be2878bed309b5ea68c50b1770700893a5a540f4
Choi SangMin <sapzil78@gmail.com>
2017-08-14 11:31:06 +0900

    #bowling 문제 풀이 1

--- bowling/src/main/scala/Bowling.scala
+++ bowling/src/main/scala/Bowling.scala
@@ -1,41 +1,31 @@
 sealed trait Bowling {
   def roll(pins: Int): Bowling = {
-    val game = this match {
-      case _ if !(0 <= pins && pins <= 10) => ErrorGame(Error("Illegal pins"))
-
+    val frame = this match {
+      case _ if !(0 <= pins && pins <= 10) => ErrorGame(Error("Illegal input pins"))
       case FinishedGame(_) => ErrorGame(Error("aready finished game"))
-
       case PlayingGame(f, Nil, acc) => PlayingGame(f, List(pins), acc)
-
       case PlayingGame(f, p1 :: Nil, acc) =>
         // strike or spear
         if (p1 == 10 || p1 + pins == 10) PlayingGame(f, List(p1, pins), acc)
-        // open
+        // open frame
         else if (p1 + pins > 10) ErrorGame(Error("frame score more than 10"))
         else PlayingGame(f + 1, Nil, acc + p1 + pins)
 
-//      case PlayingGame(10, p1 :: p2 :: Nil, acc) =>
-//        // strike!
-//        if (p1 == 10) {
-//          if (p2 == 10 || p2 + pins <= 10) FinishedGame(acc + p1 + p2 + pins)
-//          else ErrorGame(Error("rolls bonus more than 10"))
-//        }
-//        else FinishedGame(acc + p1 + p2 + pins)
-
       case PlayingGame(f, p1 :: p2 :: Nil, acc) =>
-        // strike!
-        if (p1 == 10) PlayingGame(f + 1, List(p2, pins), acc + p1 + p2 + pins)
-        // spear!
+        // strike
+        if (p1 == 10) {
+          if (p2 == 10 || p2 + pins <= 10) PlayingGame(f + 1, List(p2, pins), acc + p1 + p2 + pins)
+          else ErrorGame(Error("rolls more than 10"))
+        }
+        // spear
         else PlayingGame(f + 1, List(pins), acc + p1 + p2 + pins)
 
       case _ => this
     }
 
-    println(game)
-
-    game match {
+    frame match {
       case PlayingGame(f, ps, acc) if f == 11 => FinishedGame(acc)
-      case _ => game
+      case _ => frame
     }
   }
 

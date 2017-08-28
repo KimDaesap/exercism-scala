56f2e9b2c1ba1a9738ae6b59d01838e797725aaf
Choi SangMin <sapzil78@gmail.com>
2017-08-28 14:18:11 +0900

    * dominoes 문제 풀이 진행중...

--- dominoes/src/main/scala/Dominoes.scala
+++ dominoes/src/main/scala/Dominoes.scala
@@ -1,8 +1,23 @@
 object Dominoes {
+  type Value = (Int, Int)
+  type ValueList = List[Value]
 
   def chain(dominoes: List[(Int, Int)]): Option[List[(Int, Int)]] = {
-    p()
+    permutations(dominoes.head._1, dominoes.head._2, dominoes)
   }
 
-  private def p(): Option[List[(Int, Int)]] = { ??? }
+  private def permutations(list: ValueList): Option[ValueList] = {
+    def loop(first: Int, last: Int, length: Int, ls: ValueList): List[ValueList] = {
+      if (length == 0) ls.map(List(_))
+      else {
+        for {
+          i <- ls
+          (l, r) = i
+          j <- loop(length - 1, ls.diff(List(i)))
+        } yield i :: j
+      }
+    }
+
+    loop(list.length - 1, list)
+  }
 }

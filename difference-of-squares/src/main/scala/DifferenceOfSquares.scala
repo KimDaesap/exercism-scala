98d97ac46295e8ab0bfdc422a828ad9a9db8fc9c
Choi SangMin <sapzil78@gmail.com>
2018-12-26 14:09:11 +0900

    Difference Of Squares 문제 풀이 완료

--- difference-of-squares/src/main/scala/DifferenceOfSquares.scala
+++ difference-of-squares/src/main/scala/DifferenceOfSquares.scala
@@ -1,8 +1,13 @@
 object DifferenceOfSquares {
 
-  def sumOfSquares(n: Int): Int = ???
+  def sumOfSquares(n: Int): Int =
+    (1 to n).foldLeft(0)((acc, x) => x * x + acc)
 
-  def squareOfSum(n: Int): Int = ???
+  def squareOfSum(n: Int): Int = {
+    val sum = (1 to n).sum
+    sum * sum
+  }
 
-  def differenceOfSquares(n: Int): Int = ???
+  def differenceOfSquares(n: Int): Int =
+    squareOfSum(n) - sumOfSquares(n)
 }
